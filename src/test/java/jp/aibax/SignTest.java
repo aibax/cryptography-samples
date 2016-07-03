852f17856133bfd0b029522220d348bffbf0cc36
Daisuke AIBA <aiba@dreamgarage.jp>
2016-07-03 20:01:08 +0900

    ファイルのデジタル署名と検証のサンプルコードを追加

--- src/test/java/jp/aibax/SignTest.java
+++ src/test/java/jp/aibax/SignTest.java
@@ -84,4 +84,80 @@ public class SignTest
             fail();
         }
     }
+
+    @Test public void testTextSignatureWithKeyFile()
+    {
+        String text = "Hello! World.";
+
+        Sign sign = new Sign();
+
+        try
+        {
+            byte[] message = text.getBytes("UTF-8");
+
+            /* キーペア生成 */
+            File publicKeyFile = new File(this.getClass().getResource("/jp/aibax/rsa/publickey.der").toURI());
+            assertTrue(publicKeyFile.exists());
+            File privateKeyFile = new File(this.getClass().getResource("/jp/aibax/rsa/privatekey.pk8").toURI());
+            assertTrue(privateKeyFile.exists());
+            KeyPair keypair = new RSA().loadKeyPairFromFile(publicKeyFile, privateKeyFile);
+
+            /* 署名の生成 */
+            byte[] signature = sign.sign(message, keypair.getPrivate());
+            System.out.println("[Signature] " + signature.length + " Bytes (" + signature.length * 8 + " bits)");
+            HexDump.dump(signature, 0, System.out, 0);
+
+            /* メッセージと署名の検証 */
+            assertTrue(sign.verify(message, signature, keypair.getPublic()));
+
+            /* 別の公開鍵によるメッセージと署名の検証 */
+            KeyPair keypairAnother = new RSA().generateKeyPair(2048);
+            assertFalse(sign.verify(message, signature, keypairAnother.getPublic()));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test public void testFileSignatureWithKeyFile()
+    {
+        Sign sign = new Sign();
+
+        try
+        {
+            File file = new File(this.getClass().getResource("/lena.tif").toURI());
+            System.out.println("file.name = " + file.getName());
+            assertTrue(file.exists());
+
+            byte[] original = FileUtils.readFileToByteArray(file);
+            String hash_original = DigestUtils.sha1Hex(original);
+            System.out.println("original: size = " + original.length + " bytes / hash = " + hash_original);
+
+            /* キーペア生成 */
+            File publicKeyFile = new File(this.getClass().getResource("/jp/aibax/rsa/publickey.der").toURI());
+            assertTrue(publicKeyFile.exists());
+            File privateKeyFile = new File(this.getClass().getResource("/jp/aibax/rsa/privatekey.pk8").toURI());
+            assertTrue(privateKeyFile.exists());
+            KeyPair keypair = new RSA().loadKeyPairFromFile(publicKeyFile, privateKeyFile);
+
+            /* 署名の生成 */
+            byte[] signature = sign.sign(original, keypair.getPrivate());
+            System.out.println("[Signature] " + signature.length + " Bytes (" + signature.length * 8 + " bits)");
+            HexDump.dump(signature, 0, System.out, 0);
+
+            /* メッセージと署名の検証 */
+            assertTrue(sign.verify(original, signature, keypair.getPublic()));
+
+            /* 別の公開鍵によるメッセージと署名の検証 */
+            KeyPair keypairAnother = new RSA().generateKeyPair(2048);
+            assertFalse(sign.verify(original, signature, keypairAnother.getPublic()));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fail();
+        }
+    }
 }
